d575c1b21ad680cfe4631dc62598fbe5ecebfde0
Hubert Darryl Daryanto <hubertdaryanto@gmail.com>
2023-11-09 06:05:46 +0700

    Initial commit

---
+++ xsis-ios-developer-test/xsis_ios_developer_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xsis_ios_developer_testApp.swift
+//  xsis-ios-developer-test
+//
+//  Created by Hubert Daryanto on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct xsis_ios_developer_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
